2209dad9d6b1e71c4256cb24c3075a23d99d7de6
722sofiaameh <sofiagabriel722@gmail.com>
2024-07-20 06:24:22 +0100

    feat: set up more page

---
+++ src/app/more/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import DashboardLayout from "@/layouts/DasboardLayout";
+
+const More = () => {
+    return (
+        <>
+        <DashboardLayout>
+            <Header pageHeading={"More"}/>
+        </DashboardLayout>
+        </>
+    )
+}
+export default More;
